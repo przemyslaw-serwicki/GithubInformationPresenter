114b8c8a3a0acbafdbe857f3936aa8d59888bc4d
przemyslaw.serwicki <przemyslaw.serwicki@synergycodes.com>
2021-04-10 14:26:40 +0200

    Add project files.

---
+++ GithubInformationPresenter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubInformationPresenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
